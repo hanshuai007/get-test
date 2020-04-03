99b2946ce7276fed784fe0159bc8b32466e2c91d
hanshuai <shuaihan0324@163.com>
2020-04-03 16:51:18 +0800

    把本地工程添加到git

---
+++ src/com/FirstClass.java
@@ -0,0 +1,16 @@
+package com;
+
+/**
+ * @Author: hanshuai
+ * @Mail: shuaihan0324@163.com
+ * @Date: 2020/4/3  16:40
+ **/
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
