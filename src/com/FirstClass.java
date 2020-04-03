c99a23642ea2eda321b4640c756a243816f8e1df
hanshuai <shuaihan0324@163.com>
2020-04-03 17:09:30 +0800

    修改FirstClass

--- src/com/FirstClass.java
+++ src/com/FirstClass.java
@@ -12,9 +12,11 @@ public class FirstClass {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        //hello test
     }
     public void test(){
         int i = 0;
         i += 10;
+        System.out.println(i);
     }
 }
